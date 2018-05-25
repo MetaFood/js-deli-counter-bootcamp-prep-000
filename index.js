428242b3b9f228bb1caf6f4348ce8ebe94aca53f
Matthew Smyth <msmyth1030@gmail.com>
2018-05-25 21:58:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var katzDeli = [];
 
+function currentLine() {
+  
+}
+
 function takeANumber(line, name) {
   line.push(name);
   
   return `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing() {
+  return 
 }
