2d7f2fd698370236594e0156334cebf819557e79
Matthew Smyth <msmyth1030@gmail.com>
2018-05-25 21:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var katzDeli = [];
 function takeANumber(line, name) {
   line.push(name);
   
-  return `Welcome, ${name}. You are number ${${katzDeliLine} + 1} in line.`;
+  return `Welcome, ${name}. You are number $} in line.`;
 }
